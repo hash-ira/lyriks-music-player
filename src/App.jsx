932c41cfca019520869fd800753b9eaa3e98e107
Yash Hingane <hingane_y@es.iitr.ac.in>
2023-12-26 19:15:29 +0530

    updated side bar

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ const App = () => {
     <div className="relative flex">
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#2f302f]">
-        <Searchbar />
-
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex lg:flex-row flex-col-reverse">
+        <div className="">
+          <Searchbar />
+        </div>
+        <div className="px-6 h-[calc(100vh-64px)] overflow-y-scroll hide-scrollbar flex lg:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
@@ -25,14 +26,14 @@ const App = () => {
               <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
           </div>
-          <div className="xl:sticky relative top-0 h-fit">
+          <div className="lg:sticky relative top-0 h-fit">
             <TopPlay />
           </div>
         </div>
       </div>
 
       {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br  from-black to-[#2f302f] backdrop-blur-lg rounded-t-3xl z-10">
           <MusicPlayer />
         </div>
       )}
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -14,17 +14,17 @@ const Searchbar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-white focus-within:text-white">
       <label htmlFor="search-field" className="sr-only">
         Search all files
       </label>
-      <div className="flex flex-row justify-start items-center">
-        <FiSearch aria-hidden="true" className="w-5 h-5 ml-4" />
+      <div className="flex flex-row justify-start items-center rounded-lg ">
+        <FiSearch aria-hidden="true" className="w-4 h-4 ml-4" />
         <input
           name="search-field"
           autoComplete="off"
           id="search-field"
-          className="flex-1 bg-transparent border-none placeholder-gray-500 outline-none text-base text-white p-4"
+          className="flex-1 bg-transparent  placeholder-white outline-none text-base text-white p-3"
           placeholder="Search"
           type="search"
           value={searchTerm}
